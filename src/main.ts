f1ed106f9a11cafe3371a6bb22dd65f638c6a2e3
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-10-15 23:24:24 +0100

    Install bootstrapping modules

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
-import { port } from './config/misc/env';
+import { environment, port } from './config/misc/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.setGlobalPrefix('/api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  if (environment !== 'production') {
+    app.use(morgan('dev'));
+  }
   await app.listen(port || 3000);
   Logger.log(`Server is running on port ${port || 3000}`, 'NestApplication');
 }
