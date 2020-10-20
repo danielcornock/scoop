5f9bcfd5226ad49325a9172a31fb4d56bd53190a
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-10-20 01:23:10 +0100

    Change Db strin

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
-import { localDatabaseUriString } from './config/misc/env';
+import { databaseUriString } from './config/misc/env';
 import { NetWorthModule } from './net-worth/net-worth.module';
 import { SettingsModule } from './settings/settings.module';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot(localDatabaseUriString),
+    MongooseModule.forRoot(databaseUriString),
     SettingsModule,
     NetWorthModule
   ],
