fdf5b4369e04fa68f42d9d6bd29d4fd4e8d66de8
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-05 22:37:32 +0000

    Update email to use png

--- src/auth/templates/lib/html.ts
+++ src/auth/templates/lib/html.ts
@@ -1,11 +1,13 @@
 export function html(content: string): string {
   return `<html style="font-family: Arial, Helvetica, sans-serif">
-  <body style="background-color: white;">
-    <div style="max-width: 800px; width: 100%; margin: 0 auto;">
-      <div style="background-color: #3657dc; padding: 50px; margin-bottom: 30px; border-radius: 10px;">
-        <img src="https://app.scoopfinance.co.uk/assets/images/scoop-email.svg" style="width: 200px; margin: 0 auto; display: block;"/>
+  <body style="margin: 0; background-color: rgb(239, 242, 252)">
+    <div style="max-width: 800px; width: 100%; margin: 0 auto; background-color: white;">
+      <div style="background-color: #3657dc; padding: 50px;">
+        <img src="https://app.scoopfinance.co.uk/assets/images/scoop-email.png" style="width: 200px; margin: 0 auto; display: block;"/>
       </div>
-      ${content}
+      <div style="padding: 12px 20px; height: 100%;">
+        ${content}
+      </div>
     </div>
   </body>
 </html>`;
