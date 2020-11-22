d5ddf67457998102b991e5bfaeee68cf1d826249
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-22 19:26:03 +0000

    Remove client from repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
+import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { databaseUriFactory } from './config/database/database-uri.factory';
@@ -10,10 +9,9 @@ import { InvestmentsModule } from './investments/investments.module';
 import { MonthlyDistributionModule } from './monthly-distribution/monthly-distribution.module';
 import { NetWorthModule } from './net-worth/net-worth.module';
 import { NotificationsModule } from './notifications/notifications.module';
-import { SettingsModule } from './settings/settings.module';
-import { AdminModule } from './admin/admin.module';
-import { TasksModule } from './tasks/tasks.module';
 import { SalaryModule } from './salary/salary.module';
+import { SettingsModule } from './settings/settings.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -23,9 +21,6 @@ import { SalaryModule } from './salary/salary.module';
     }),
     SettingsModule,
     NetWorthModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client')
-    }),
     InvestmentsModule,
     MonthlyDistributionModule,
     NotificationsModule,
