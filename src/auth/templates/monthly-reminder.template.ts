f2f976d8f24572dd10b9c9ba0957bbf82226ff11
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-11-28 19:29:50 +0000

    Change words on reminder email

--- src/auth/templates/monthly-reminder.template.ts
+++ src/auth/templates/monthly-reminder.template.ts
@@ -3,9 +3,9 @@ import { buttonStyles } from './lib/button-styles';
 import { linkStyles } from './lib/link-styles';
 
 export function getMonthlyReminderTemplate(name: string): string {
-  return `<p>Hi ${name.split(' ')[0]}!</p>
+  return `<p>Hi ${name.split(' ')[0]},</p>
 <p>It's Scoop day!</p>
-<p>Just a friendly reminder that a month has passed, and it's time for you to update your logs!</p>
+<p>Just a friendly reminder that a month has passed, and it's time for you to update your logs.</p>
 <p><a href="${appUrl('')}" ${buttonStyles()}>Go to application</a></p>
 <p>If you don't want to receive these emails or want to change the date that you receive this reminder, you can head to the <a ${linkStyles()} href="${appUrl(
     'user-settings'
