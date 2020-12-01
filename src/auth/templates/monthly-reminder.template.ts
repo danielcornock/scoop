dd707b156adebce697573ffde41947f17fe88350
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-12-01 00:47:20 +0000

    Update monthly reminder email template

--- src/auth/templates/monthly-reminder.template.ts
+++ src/auth/templates/monthly-reminder.template.ts
@@ -3,13 +3,17 @@ import { buttonStyles } from './lib/button-styles';
 import { linkStyles } from './lib/link-styles';
 
 export function getMonthlyReminderTemplate(name: string): string {
-  return `<p>Hi ${name.split(' ')[0]},</p>
-<p>It's Scoop day! 💎</p>
-<p>Just a friendly reminder that a month has passed, and it's time for you to update your logs.</p>
-<p><a href="${appUrl('')}" ${buttonStyles()}>Go to application</a></p>
-<p>If you don't want to receive these emails or want to change the date that you receive this reminder, you can head to the <a ${linkStyles()} href="${appUrl(
+  return `<p>Hi, ${name.split(' ')[0]}! 👋</p>
+<p>It's your personal Scoop day 💎</p>
+<p>Just a friendly reminder that a month has passed, and now would be the perfect time for you to hop on and update your logs!</p>
+<p>If you haven't been online much lately you'll be pleased to find a load of new features, including a whole new section for tracking your salary, net worth projection, and a complete overhaul of the mobile interface.</p>
+<p style="text-align: center"><a href="${appUrl(
+    ''
+  )}" ${buttonStyles()}>Go to application</a></p>
+<p>As always, thanks for being a part of Scoop - let's take control of our finances together 💪</p>
+<p>If you don't want to receive these emails or would like to change the date that you receive this reminder, you can head to the <a ${linkStyles()} href="${appUrl(
     'user-settings'
-  )}"/>user settings page</a> and update your settings.</p>
+  )}"/>user settings page</a> and update your preferences.</p>
 <div>Best wishes,</div>
 <div>The Scoop Team</div>
 `;
